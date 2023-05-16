9e24c86e5ea870ee6c0fbc7712cbe29a57db8f19
Leandro Arguello <leandro.arguello.99@gmail.com>
2023-05-16 18:54:17 -0500

    Primera version, consulta los 5 primeros registros

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = await client.GetAsync(API_URL);
+        string content = await response.Content.ReadAsStringAsync();
+        List<Post> posts = JsonSerializer.Deserialize<List<Post>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? new List<Post>();
+
+        foreach (Post post in posts)
+        {
+            Console.WriteLine($"ID: {post.Id}, Title: {post.Title}");
+        }
+    }
+}
+
+class Post
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string? Title { get; set; }
+    public string? Body { get; set; }
+}
