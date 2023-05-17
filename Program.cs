c127186cc7060729267a9aa30721cf500565b908
Leandro Arguello <leandro.arguello.99@gmail.com>
2023-05-16 21:56:25 -0500

    se pueden consultar mas posts

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static async Task Main(string[] args)
+    public static async Task getPosts(int start, int limit)
     {
-        string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+        string API_URL = $"https://jsonplaceholder.typicode.com/posts?_start={start}&_limit={limit}";
         HttpClient client = new HttpClient();
         HttpResponseMessage response = await client.GetAsync(API_URL);
         string content = await response.Content.ReadAsStringAsync();
@@ -20,7 +20,36 @@ class Program
             Console.WriteLine($"ID: {post.Id}, Title: {post.Title}");
         }
     }
+
+    static async Task Main(string[] args)
+    {
+        Boolean continuar = true;
+        int start = 0;
+        int limit = 5;
+        do
+        {
+            Console.WriteLine("Escoja una opcion :");
+            Console.WriteLine($"1. Ver los post desde {start} hasta {limit}:");
+            Console.WriteLine($"2. Terminar la ejecucion :");
+            string entrada = Console.ReadLine() ?? "2";
+            switch (entrada)
+            {
+                case "1":
+                    await getPosts(start, 5);
+                    start += 5;
+                    limit += 5;
+                    break;
+                case "2":
+                    continuar = false;
+                    break;
+                default:
+                    Console.WriteLine("No ha seleccionado ninguna de las opciones");
+                    break;
+            }
+        } while (continuar);
+    }
 }
+
 
 class Post
 {
